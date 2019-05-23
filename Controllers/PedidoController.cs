4181012c00a84b54176b1cc8d8a09d5b0531ae11
Pedro Henrique Souza De Queiroz <38128442813@info.local>
2019-05-23 17:32:15 -0300

    oi

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hamburgueria_Tarde.Controllers
+{
+    public class PedidoController : Controller{
+    
+        
+    
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
